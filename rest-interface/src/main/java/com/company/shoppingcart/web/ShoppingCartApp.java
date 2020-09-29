67e2cd8c8416ef9c5f78d9a667f509ae6c063314
Yasin Bhojawala <yasin.bhojawala@gmail.com>
2020-09-29 17:52:15 +0530

    Rename the class

--- rest-interface/src/main/java/com/company/shoppingcart/web/ShoppingCartApp.java
+++ rest-interface/src/main/java/com/company/shoppingcart/web/ShoppingCartApp.java
@@ -1,7 +1,6 @@
 package com.company.shoppingcart.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,9 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "com.company")
 @EnableJpaRepositories(basePackages = "com.company")
 @EntityScan("com.company")
-public class ShopizerApp {
+public class ShoppingCartApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopizerApp.class, args);
+        SpringApplication.run(ShoppingCartApp.class, args);
     }
 }
